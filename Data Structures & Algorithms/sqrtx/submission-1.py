adf60b14bc1eea5b78948eff0add9f4772926cd1
web10-bariflo <web10.bariflo@outlook.com>
2026-04-02 16:19:22 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        low,high = 1,x
+        while low <= high:
+            mid = (low + high) // 2
+
+            if mid * mid  == x:
+                return mid
+            elif mid * mid < x:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return high
+
