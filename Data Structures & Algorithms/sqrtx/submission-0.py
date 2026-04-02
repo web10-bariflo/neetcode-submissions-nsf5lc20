99ae2bd3daa2523b0bf126fd454565e773358676
web10-bariflo <web10.bariflo@outlook.com>
2026-04-02 16:10:30 +0530

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.py
@@ -0,0 +1,21 @@
+# class Solution:
+#     def mySqrt(self, x: int) -> int:
+#         if x == 0:
+#             return 0
+#         low,high = 1,x
+#         while low <= high:
+#             mid = (low + high) // 2
+
+#             if mid * mid  == x:
+#                 return mid
+#             elif mid * mid < x:
+#                 low = mid + 1
+#             else:
+#                 high = mid - 1
+#         return high
+
+
+class Solution:
+     def mySqrt(self, x: int) -> int:
+        root = x**0.5
+        return int(root)
